3c6d814d3b0a760b0b18f0c66eee441a09e08428
KSN <v999vvv@naver.com>
2022-09-20 00:23:51 +0900

    baekjoon 4673 셀프넘버

---
+++ 함수_4673_셀프넘버.py
@@ -0,0 +1,10 @@
+numbers = set(range(1,10001))
+num_d=set()
+for i in range(1,10001):
+    for j in str(i):
+        i +=int(j)
+        num_d.add(i)
+
+self_num = sorted(numbers - num_d)
+for i in self_num:
+    print(i)
