4c40ba99197c3b1c59c29c4eabf9df0b75968965
KSN <v999vvv@naver.com>
2022-09-21 00:13:49 +0900

    1065_한수_백준

--- 함수_4673_셀프넘버.py
+++ 함수_4673_셀프넘버.py
@@ -3,8 +3,8 @@ num_d=set()
 for i in range(1,10001):
     for j in str(i):
         i +=int(j)
-        num_d.add(i)
+    num_d.add(i)
 
 self_num = sorted(numbers - num_d)
-for i in self_num:
-    print(i)
+for k in self_num:
+    print(k)
