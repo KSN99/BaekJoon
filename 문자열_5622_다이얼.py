60bde030b089246b53ed8b4cae551760d7189670
KSN <v999vvv@naver.com>
2022-10-18 02:51:27 +0900

    baekjoon

--- 문자열_5622_다이얼.py
+++ 문자열_5622_다이얼.py
@@ -1,10 +1,20 @@
-alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
-word = input()
-
-time = 0
-for unit in alpabet_list :  
-    for i in unit:  # alpabet 리스트에서 각 요소를 꺼내서 한글자씩 분리
-        for x in word :  # 입력받은 문자를 하나씩 분리
-            if i == x :  # 두 알파벳이 같으면
-                time += alpabet_list.index(unit) +3  # time = time + index +3
-print(time)
+word=input()
+min_time=0
+for i in word:
+    if i =='A' or i=='B' or i=='C':
+        min_time+=3
+    elif i == 'D' or i=='E' or i=='F':
+        min_time+=4
+    elif i == 'G' or i=='H' or i=='I':
+        min_time+=5
+    elif i == 'J' or i=='K' or i=='L':
+        min_time+=6
+    elif i == 'M' or i=='N' or i=='O':
+        min_time+=7
+    elif i == 'P' or i=='Q' or i=='R' or i=='S':
+        min_time+=8
+    elif i == 'T' or i=='U' or i=='V':
+        min_time+=9
+    elif i== 'W' or i=='X' or i=='Y' or i=='Z':
+        min_time+=10 
+print(min_time)
