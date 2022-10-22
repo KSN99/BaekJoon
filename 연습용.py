573a56c84e363d87e6947453e7f2bb03bce62e4a
KSN <v999vvv@naver.com>
2022-10-22 16:42:49 +0900

    baekjoon

--- 연습용.py
+++ 연습용.py
@@ -1,12 +1,9 @@
-C=int(input())
-
-for i in range(C):
-    scores=list(map(int,input().split()))
-    avg=sum(scores[1:])/scores[0]
-    avg_student=[]
-    for i in range(1,scores[0]+1):
-        if scores[i]>avg:
-            avg_student.append(scores[i]) # [80 100 ]
-    new_avg=len(avg_student)/scores[0] * 100 
-    print(f'{new_avg:.3f}%')
-        
+dial=['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word=input()
+cnt=0
+for i in dial:
+    for k in word:
+        if k in i:
+            c=dial.index(i)+3
+            cnt+=c
+print(cnt)
