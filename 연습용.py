69f54c3debe03cee01185228197b107ff3c7f65b
KSN <v999vvv@naver.com>
2022-12-02 18:56:18 +0900

    ..

--- 연습용.py
+++ 연습용.py
@@ -1,8 +1,20 @@
-nums=[]
-for i in range(28):
-    n=int(input())
-    nums.append(n)
+def linear_search(x,ky):
+    for i in range(num):
+        if x[i]==ky:
+            return i
+    return -1
 
-for k in range(1,31):
-    if k not in nums:
-        print(k)
+if __name__=='__main__':
+    num=int(input("원소 수를 입력: "))
+    x=[None] * num
+    for i in range(num):
+        x[i]=int(input(f'x[{i}]: '))
+    
+    ky=int(input(f'검색할 값을 입력하세요: '))
+
+    idx=linear_search(x,ky)
+
+    if idx==-1:
+        print(f'원소값 존재 x')
+    else:
+        print(f'검색값은 x[{idx}]에 있습니다')
