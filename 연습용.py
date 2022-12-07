c2af16ad4ddaf4372e4fd586143834d7e6b38dad
KSN <v999vvv@naver.com>
2022-12-07 18:43:30 +0900

    baekjoon

--- 연습용.py
+++ 연습용.py
@@ -1,20 +1,8 @@
-def linear_search(x,ky):
-    for i in range(num):
-        if x[i]==ky:
-            return i
-    return -1
+def F(n):
+    if n<=1:
+        return n
+    return F(n-1) + F(n-2)
+ 
 
-if __name__=='__main__':
-    num=int(input("원소 수를 입력: "))
-    x=[None] * num
-    for i in range(num):
-        x[i]=int(input(f'x[{i}]: '))
-    
-    ky=int(input(f'검색할 값을 입력하세요: '))
-
-    idx=linear_search(x,ky)
-
-    if idx==-1:
-        print(f'원소값 존재 x')
-    else:
-        print(f'검색값은 x[{idx}]에 있습니다')
+n=int(input())
+print(F(n))
