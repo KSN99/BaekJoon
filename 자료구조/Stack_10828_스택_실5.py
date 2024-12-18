a18ce8eed2e5ee7fde34ca01b30f52060f380797
KSN99 <v999vvv@naer.com>
2024-12-19 00:30:33 +0900

    Stack_10828_스택_실5.py (SOLVED)

--- 자료구조/Stack_10828_스택_실5.py
+++ 자료구조/Stack_10828_스택_실5.py
@@ -1,49 +1,35 @@
-class Stack():
-    def __init__(self):
-        self.items = []
-        self.top = -1
+import sys
 
-    def push(self, item):
-        self.items.append(item)
-        self.top += 1
+N = int(sys.stdin.readline())
+stack = []
+# 1. push : 정수 X를ㄹ 스택에 넣는 연산
+# 2. pop : 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# 3. size : 스택에 들어있는 정수의 개수를 출력한다.
+# 4. empty : 스택이 비어있으면 1, 아니면 0을 출력한다.
+# 5. top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# input 2, if문을 활용한 스택 기능 구현
 
-    def pop(self):
-        if self.is_empty() == 1:
-            print(-1)
-        else:
-            self.items.pop()
-            print(self.items[::-1])
-            self.top -= 1
-
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        if self.top == -1:
-            return 1
-        else: return 0
-    def peek(self):
-        if self.is_empty() == 1:
-            print(-1)
-        else:
-            print(self.items[self.top])
-
-stack = Stack()
-X = int(input())
-for i in range(X):
-    stack_cmd = input()
-    cmd_num = stack_cmd.split()
-    if "push" in stack_cmd:
-        stack.push(int(cmd_num[1]))
-
-    if "top" in stack_cmd:
-        stack.peek()
-
-    if "pop" in stack_cmd:
-        stack.pop()
-
-    if "size" in stack_cmd:
-        stack.size()
-
-    if "empty" in stack_cmd:
-        stack.is_empty()
+for i in range(N):
+    """리스트 출력"""
+    stack_cmd = sys.stdin.readline().split()
+    """push"""
+    if stack_cmd[0] == "push":
+        stack.append(int(stack_cmd[1]))
+    """pop"""
+    if stack_cmd[0] == "pop":
+        if len(stack) > 0 :
+            print(stack[-1])
+            stack.pop()
+        else: print(-1)
+    """size"""
+    if stack_cmd[0] == "size":
+        print(len(stack))
+    """empty"""
+    if stack_cmd[0] == "empty":
+        if len(stack) == 0 : print(1)
+        else: print(0)
+    """top"""
+    if stack_cmd[0] == """top""":
+        if len(stack) > 0 :
+            print(stack[-1])
+        else: print(-1)
